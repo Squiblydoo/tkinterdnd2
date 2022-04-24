a7f65225f30ac8d8363ba54bf93bd7a4591d1597
✨小透明・宸✨ <47057319+TransparentLC@users.noreply.github.com>
2022-04-24 17:05:11 +0800

    Exclude unused platform-specific files in pyinstaller hook

--- hook-tkinterdnd2.py
+++ hook-tkinterdnd2.py
@@ -6,7 +6,17 @@ Just put hook-tkinterdnd2.py in the same directory where you call pyinstaller an
     pyinstaller myproject/myproject.py --additional-hooks-dir=.
 """
 
-from PyInstaller.utils.hooks import collect_data_files, eval_statement
+import os
+import platform
+from PyInstaller.utils.hooks import collect_data_files
 
-
-datas = collect_data_files('tkinterdnd2')
+s = platform.system()
+p = {
+    'Windows': 'win64',
+    'Linux': 'linux64',
+    'Darwin': 'osx64',
+}
+if s in p:
+    datas = [x for x in collect_data_files('tkinterdnd2') if os.path.split(x[1])[1] == p[s]]
+else:
+    raise RuntimeError(f'TkinterDnD2 is not supported on platform "{s}".')
